c4bf1e7683b382aeea96ba4afd592dd42009c7ae
shidatuo <1003230257@qq.com>
2023-09-13 10:24:09 +0800

    提交代码 , 解决问题

--- src/Models/Orders/OrderList.php
+++ src/Models/Orders/OrderList.php
@@ -163,7 +163,7 @@ class OrderList implements ModelInterface, ArrayAccess, IterableType
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = parent::listInvalidProperties();
+        $invalidProperties = [];
 
         return $invalidProperties;
     }
